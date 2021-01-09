f150a92e091c80e2a36cc09e9dc341ec0907ccc3
csrausyan <csrausyan@gmail.com>
2021-01-09 10:52:54 +0700

    change some

--- Assets/scoreBasketball.cs
+++ Assets/scoreBasketball.cs
@@ -6,14 +6,20 @@ public class scoreBasketball : MonoBehaviour
 {
     public GameObject trigger;
     public TextMesh score;
+    int value = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        score.text = value.ToString();
+        Debug.Log("inisiasi score");
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("the ball has enter the ring");
+        value += 1;
+        score.text = value.ToString();
         //speed = speed * -1;
         //colorPicker = Random.Range(0, 10);
     }
